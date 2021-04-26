50f2c9bfa71f6e5acc3de532e84309bc43260fa3
Daisy Lee <lspss93055@gmail.com>
2021-04-27 02:04:33 +0800

    Signin API done

--- src/app/users/signin/signin.component.ts
+++ src/app/users/signin/signin.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from 'app/auth/auth.service';
 import { Router } from 'app/testing/router-stubs';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-signin',
@@ -11,14 +12,14 @@ import { Router } from 'app/testing/router-stubs';
 export class SigninComponent implements OnInit {
   signinForm: FormGroup;
   private errorMessage: string;
-  userName: string;
+  username: string;
   password: string;
 
-  constructor(private router: Router,private authService: AuthService) { }
+  constructor(private router: Router,private userService: UserService,private authService: AuthService) { }
 
   ngOnInit() {
     this.signinForm = new FormGroup({
-      userName: new FormControl("admin"),
+      username: new FormControl("admin"),
       password: new FormControl("admin"),
     });
   //   this.signinForm = this.formBuilder.group({
@@ -26,25 +27,42 @@ export class SigninComponent implements OnInit {
   //     password: ['', [Validators.required, Validators.minLength(6)]],
   // });
   }
-
- 
   onSubmit(data: any) {
-    this.userName = data.userName;
+    this.username = data.username;
     this.password = data.password;
+    console.log(data)
 
-    console.log("Login page: " + this.userName);
+    console.log("Login page: " + this.username);
     console.log("Login page: " + this.password);
 
-    this.authService.signin(this.userName, this.password)
+    this.userService.signIn(data)
        .subscribe( data => { 
           console.log("Is Login Success: " + data);
           if(data)
-            this.router.navigate(['/']);
-          else
             this.router.navigate(['/appointment']);
+          else
+            this.router.navigate(['/']);
           
         });
   }
+ 
+  // onSubmit(data: any) {
+  //   this.userName = data.userName;
+  //   this.password = data.password;
+
+  //   console.log("Login page: " + this.userName);
+  //   console.log("Login page: " + this.password);
+
+  //   this.authService.signin(this.userName, this.password)
+  //      .subscribe( data => { 
+  //         console.log("Is Login Success: " + data);
+  //         if(data)
+  //           this.router.navigate(['/']);
+  //         else
+  //           this.router.navigate(['/appointment']);
+          
+  //       });
+  // }
   gotoRegister(){
     this.router.navigate(['/signup']);
   }
