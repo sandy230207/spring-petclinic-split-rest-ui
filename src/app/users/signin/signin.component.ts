0c4999b62c043b983c9c2e0a1c09ab1c75beba91
Daisy Lee <lspss93055@gmail.com>
2021-05-24 12:02:50 +0800

    modify router

--- src/app/users/signin/signin.component.ts
+++ src/app/users/signin/signin.component.ts
@@ -56,7 +56,7 @@ export class SigninComponent implements OnInit {
         .pipe(first())
         .subscribe(data => {
           this.refreshPage()
-          this.router.navigate(['/welcome']);
+          this.router.navigate(['/']);
         });
     
     } else {
@@ -64,7 +64,7 @@ export class SigninComponent implements OnInit {
         .pipe(first())
         .subscribe(data => {
           this.refreshPage()
-          this.router.navigate(['/welcome']);
+          this.router.navigate(['/']);
           // this.router.navigate(['owners/appointments', 12, 'now']);
 
        
