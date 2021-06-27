9ff96acde8800a43d3b0d8562c99fda3d222d0e6
Daisy Lee <lspss93055@gmail.com>
2021-06-28 00:37:28 +0800

    change owner and vet router to ./appointment

--- src/app/users/signin/signin.component.ts
+++ src/app/users/signin/signin.component.ts
@@ -83,7 +83,7 @@ export class SigninComponent implements OnInit {
     this.router.navigate(['/']);
   }
   refreshPage() {
-    window.location.replace('/');
+    window.location.replace('/appointment');
   }
 
 
